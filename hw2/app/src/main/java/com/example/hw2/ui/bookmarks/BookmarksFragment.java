a095a9de55d42761a77dc5ae2c25f910f8eee9e2
amdigbari <amirmahdidigbari@yahoo.com>
2021-04-16 20:10:21 +0430

    feat(hw2): add bottom navigation bar

---
+++ hw2/app/src/main/java/com/example/hw2/ui/bookmarks/BookmarksFragment.java
@@ -0,0 +1,28 @@
+package com.example.hw2.ui.bookmarks;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.hw2.R;
+
+public class BookmarksFragment extends Fragment {
+
+    private BookmarksViewModel bookmarksViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        bookmarksViewModel =
+                new ViewModelProvider(this).get(BookmarksViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_bookmarks, container, false);
+        final TextView textView = root.findViewById(R.id.text_bookmarks);
+        bookmarksViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+}
---
+++ hw2/app/src/main/java/com/example/hw2/ui/map/MapFragment.java
@@ -0,0 +1,28 @@
+package com.example.hw2.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.hw2.R;
+
+public class MapFragment extends Fragment {
+
+    private MapViewModel mapViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        mapViewModel =
+                new ViewModelProvider(this).get(MapViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+        final TextView textView = root.findViewById(R.id.text_map);
+        mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+}
